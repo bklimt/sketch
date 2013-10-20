7fc60883adc8510e55b573ecfafe17b9d6693c0d
Bryan Klimt <bklimt@gmail.com>
2013-10-19 17:14:28 -0700

    Rescale large images.

--- cloud/main.js
+++ cloud/main.js
@@ -14,6 +14,27 @@ Parse.Cloud.beforeSave("ImageFile", function(request, response) {
     return image.setData(response.buffer);
 
   }).then(function(image) {
+    if (image.width() <= 600 && image.height() <= 400) {
+      return image;
+    }
+
+    var width = image.width();
+    var height = image.height();
+    var ratio = width / height;
+
+    if (width > 600) {
+      width = 600;
+      height = Math.floor(width / ratio);
+    }
+
+    if (height > 400) {
+      height = 400;
+      width = Math.floor(height * ratio);
+    }
+
+    return image.scale({ width: width, height: height });
+
+  }).then(function(image) {
     request.object.set("width", image.width());
     request.object.set("height", image.height());
     request.object.set("format", image.format());
